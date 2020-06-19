222a84125dde5e875f7a076f16f5375dd5e7f4df
Juan Falcón <JUANCANEN_10_@HOTMAIL.COM>
2020-06-18 21:18:17 -0500

    added auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+const { verify } = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config');
+
+module.exports = (req, res, next) => {
+    const token = req.headers["authorization"];
+    if (!token) {
+        const error = new Error();
+        error.status = 400;
+        error.message = 'No ingreso el token';
+        throw error;
+    }
+
+    verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+            const error = new Error();
+            error.status = 401;
+            error.message = "El token ingresado es inválido";
+            throw error;
+        }
+        req.user = decoded.token;
+        next();
+    });
+
+}
